8b973af4517e7be56299fe5e410bc4221b8de684
Victor Lupascu <sinajeen@gmail.com>
2021-09-05 01:08:31 -0500

    add pizza validator

--- models/Pizza.js
+++ models/Pizza.js
@@ -2,37 +2,43 @@ const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema(
-    {
-        pizzaName: {
-            type: String
-        },
-        createdBy: {
-            type: String
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (createdAtVal) => dateFormat(createdAtVal)
-        },
-        size: {
-            type: String,
-            default: 'Large'
-        },
-        toppings: [],
-        comments: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Comment'
-            }
-        ]
+  {
+    pizzaName: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        id: false
-    }
+    createdBy: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    size: {
+      type: String,
+      required: true,
+      enum: ["Personal", "Small", "Medium", "Large", "Extra Large"],
+      default: "Large",
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
 );
 
 // get total count of comments and replies on retrieval
