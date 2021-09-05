fef3dfd590474cd62a91e7a3786f0d235a21d1d3
Victor Lupascu <sinajeen@gmail.com>
2021-09-05 01:00:10 -0500

    add indexedDB

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -52,21 +52,22 @@ const handlePizzaSubmit = event => {
 
   const formData = { pizzaName, createdBy, size, toppings };
   
-  fetch('/api/pizzas', {
-    method: 'POST',
+  fetch("/api/pizzas", {
+    method: "POST",
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(formData)
+    body: JSON.stringify(formData),
   })
-    .then(response => response.json())
-    .then(postResponse => {
-      alert('Pizza created successfully!');
+    .then((response) => response.json())
+    .then((postResponse) => {
+      alert("Pizza created successfully!");
       console.log(postResponse);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
+      saveRecord(formData);
     });
 };
 
